a0fce2d04703b862380667a9f2398ad794521a45
Wouter Wisse <hello@wouterwisse.com>
2022-05-18 16:01:26 -0400

    Setup concept layout

--- EmojiShoppingList/EmojiShoppingList/Source/ContentView.swift
+++ EmojiShoppingList/EmojiShoppingList/Source/ContentView.swift
@@ -1,10 +1,3 @@
-//
-//  ContentView.swift
-//  EmojiShoppingList
-//
-//  Created by Wouter Wisse on 17/05/2022.
-//
-
 import SwiftUI
 import CoreData
 
@@ -15,18 +8,62 @@ struct ContentView: View {
         sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
         animation: .default)
     private var items: FetchedResults<Item>
+    
+    @State private var text: String = ""
+    @FocusState private var isAddItemTextFieldFocussed: Bool
 
     var body: some View {
         NavigationView {
             List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
+                
+                // Search
+                HStack(spacing: 12) {
+                    Text("🛒")
+                        .font(.title2)
+                        .frame(width: 50, height: 50, alignment: .center)
+                        .background(.gray.opacity(0.1))
+                        .multilineTextAlignment(.center)
+                        .cornerRadius(25)
+                    
+                    TextField("", text: $text, prompt: Text("Add new item"))
+                    .font(.headline)
+                    .focused($isAddItemTextFieldFocussed)
+                    .onSubmit {
+                        addItem()
+                        text = ""
+                        isAddItemTextFieldFocussed = true
+                    }
+                    
+                    Spacer()
+                    
+                    if isAddItemTextFieldFocussed == true {
+                        Button {
+                            withAnimation {
+                                isAddItemTextFieldFocussed = false
+                            }
+                        } label: {
+                            HStack {
+                                Text("✓")
+                                Image(systemName: "keyboard.chevron.compact.down")
+                            }
+                        }
+                        .frame(width: 60, height: 30, alignment: .center)
+                        .foregroundColor(.white)
+                        .background(.green)
+                        .cornerRadius(6)
                     }
                 }
+                .listRowSeparatorTint(.clear)
+                .listRowBackground(Color.clear)
+                    
+                ForEach(items) { item in
+                    ListItem(title: "Avocado", emoji: "🥑", color: .green)
+                        .frame(minHeight: 50, maxHeight: 50)
+                }
                 .onDelete(perform: deleteItems)
+                .onMove(perform: move)
+                .listRowInsets(.init(top: 16, leading: 16, bottom: 16, trailing: 16))
+                .listRowBackground(Color.clear)
             }
             .toolbar {
                 ToolbarItem(placement: .navigationBarTrailing) {
@@ -38,8 +75,13 @@ struct ContentView: View {
                     }
                 }
             }
-            Text("Select an item")
+            .listStyle(.plain)
+            .navigationTitle("Shopping 🛍")
         }
+    }
+    
+    private func move(from source: IndexSet, to destination: Int) {
+        print("Move items")
     }
 
     private func addItem() {
@@ -74,6 +116,42 @@ struct ContentView: View {
     }
 }
 
+struct ListItem: View {
+    let title: String
+    let emoji: String
+    let color: Color
+    @State private var amount: Int = 1
+    
+    private let backgroundColor: Color = .gray
+    private let buttonColor: Color = .accentColor
+    
+    var body: some View {
+        HStack(spacing: 12) {
+            Text(emoji)
+                .font(.title2)
+                .frame(width: 50, height: 50, alignment: .center)
+                .background(color.opacity(0.1))
+                .multilineTextAlignment(.center)
+                .cornerRadius(25)
+            
+            Text(title)
+                .font(.headline)
+            
+            Spacer()
+            
+            HStack(spacing: 8) {
+                Text("\(self.amount)")
+                    .font(.caption)
+                    .fontWeight(.semibold)
+                    .frame(width: 20, alignment: .center)
+                
+                Stepper("Step", onIncrement: { amount += 1 }, onDecrement: { amount -= 1 })
+                    .labelsHidden()
+            }
+        }
+    }
+}
+
 private let itemFormatter: DateFormatter = {
     let formatter = DateFormatter()
     formatter.dateStyle = .short
@@ -82,7 +160,15 @@ private let itemFormatter: DateFormatter = {
 }()
 
 struct ContentView_Previews: PreviewProvider {
+    static let colorSchemes: [ColorScheme] = [.light, .dark]
+    
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        ForEach(colorSchemes, id: \.self, content: { colorScheme in
+            Group {
+                ContentView()
+                    .preferredColorScheme(colorScheme)
+                    .environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+            }
+        })
     }
 }
