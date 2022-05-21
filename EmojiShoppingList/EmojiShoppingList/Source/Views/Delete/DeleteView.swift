455607be0d6c902d5119de50542bab77a15f9f81
Wouter Wisse <hello@wouterwisse.com>
2022-05-21 15:00:34 -0400

    Add content for delete view

--- EmojiShoppingList/EmojiShoppingList/Source/Views/Delete/DeleteView.swift
+++ EmojiShoppingList/EmojiShoppingList/Source/Views/Delete/DeleteView.swift
@@ -2,7 +2,31 @@ import SwiftUI
 
 struct DeleteView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 12) {
+            Button(role: .destructive) {
+                // Delete all
+            } label: {
+                Text("Delete all items from list")
+                    .frame(maxWidth: .infinity, alignment: .center)
+            }
+            .buttonStyle(.borderedProminent)
+            
+            Button {
+                // Delete all
+            } label: {
+                Text("Delete all striked items from list")
+                    .frame(maxWidth: .infinity, alignment: .center)
+            }
+            .buttonStyle(.bordered)
+            
+            Button {
+                // Delete all
+            } label: {
+                Text("Cancel")
+                    .frame(maxWidth: .infinity, alignment: .center)
+            }
+            .buttonStyle(.plain)
+        }
     }
 }
 
@@ -12,6 +36,7 @@ struct DeleteView_Previews: PreviewProvider {
     static var previews: some View {
         ForEach(colorSchemes, id: \.self) { colorScheme in
             DeleteView()
+                .padding()
                 .preferredColorScheme(colorScheme)
         }
     }
