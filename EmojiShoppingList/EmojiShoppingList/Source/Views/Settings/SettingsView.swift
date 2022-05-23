e43f401b833405781f7c3a33a21f564af9c8f684
Wouter Wisse <hello@wouterwisse.com>
2022-05-23 17:26:57 -0400

    Create settings view layout

--- EmojiShoppingList/EmojiShoppingList/Source/Views/Settings/SettingsView.swift
+++ EmojiShoppingList/EmojiShoppingList/Source/Views/Settings/SettingsView.swift
@@ -4,35 +4,62 @@ struct SettingsView: View {
     var body: some View {
         NavigationView {
             List {
-                VStack(spacing: 8) {
-                    Image("Icon")
-                        .resizable()
-                        .frame(width: 100, height: 100, alignment: .center)
-                    Text("Emoji Shopping List")
-                        .font(.headline)
-                    Text("1.0")
-                        .font(.caption)
+                Section {
+                    SettingsItemListView(emoji: "📱", color: .gray, title: "Change app icon")
+                    SettingsItemListView(emoji: "🤩", color: .green, title: "Rate on AppStore")
                 }
-                .listRowSeparatorTint(.clear)
-                .frame(maxWidth: .infinity, minHeight: 200, maxHeight: 200, alignment: .center)
-                
-                Group {
-                    VStack(alignment: .leading, spacing: 4) {
-                        Text("Consider a donation")
-                            .font(.headline)
-                        Text("Creating and maintaining an app is a lot of work. I have build this app for you, for free. But just like you, I also need to buy groceries. 😊")
-                            .font(.caption)
-                    }
-                    .listRowSeparatorTint(.clear)
+
+                Section("Consider a donation") {
                     DonateListView(emoji: "🍏", color: .green, title: "Buy me an apple", price: "$0.99")
-                    DonateListView(emoji: "☕️", color: .brown, title: "Buy me a coffee", price: "$2.99")
-                    DonateListView(emoji: "🍺", color: .yellow, title: "Buy me a beer", price: "$4.99")
+                    DonateListView(emoji: "☕️", color: .green, title: "Buy me a coffee", price: "$2.99")
+                    DonateListView(emoji: "🍺", color: .green, title: "Buy me a beer", price: "$4.99")
                 }
                 
+                Section {
+                    VStack {
+                        Image("Icon")
+                            .resizable()
+                            .frame(width: 60, height: 60, alignment: .center)
+                            .cornerRadius(12)
+                        Text("1.0")
+                            .font(.caption)
+                        
+                        Button {
+                            // Do something
+                        } label: {
+                            Text("🐦 @WouterWisse")
+                        }
+                        .tint(.blue)
+                        .buttonStyle(.bordered)
+                        .controlSize(.small)
+                    }
+                    .frame(maxWidth: .infinity, alignment: .center)
+                }
+                .listRowBackground(Color.clear)
             }
-            .listStyle(.plain)
-            .navigationTitle("Settings")
+            .listStyle(.insetGrouped)
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationTitle("Emoji Shopping List")
         }
+    }
+}
+
+struct SettingsItemListView: View {
+    let emoji: String
+    let color: Color
+    let title: String
+    
+    var body: some View {
+        HStack(spacing: 12) {
+            RoundEmojiView(
+                emoji: emoji,
+                color: color
+            )
+            
+            Text(title)
+                .font(.headline)
+        }
+        .padding(.vertical, 8)
     }
 }
 
@@ -69,7 +96,13 @@ struct DonateListView: View {
 }
 
 struct SettingsView_Previews: PreviewProvider {
+    static let colorSchemes: [ColorScheme] = [.light, .dark]
+    
     static var previews: some View {
-        SettingsView()
+        ForEach(colorSchemes, id: \.self) { colorScheme in
+            SettingsView()
+                .padding()
+                .preferredColorScheme(colorScheme)
+        }
     }
 }
