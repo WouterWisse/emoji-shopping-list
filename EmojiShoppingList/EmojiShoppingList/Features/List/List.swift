c033ded02b9c15162da200b409288f33a354f0b5
Wouter Wisse <hello@wouterwisse.com>
2022-05-27 16:12:05 -0400

    Create list TCA setup

--- EmojiShoppingList/EmojiShoppingList/Features/List/List.swift
+++ EmojiShoppingList/EmojiShoppingList/Features/List/List.swift
@@ -1,19 +1,43 @@
 import ComposableArchitecture
 
 struct ListState: Equatable {
-    
+    var items: [ListItem] = []
+    var isSettingsPresented: Bool = false
+    var isDeletePresented: Bool = false
 }
 
 enum ListAction: Equatable {
-    
+    case onAppear
+    case fetched(items: [ListItem])
+    case settingsButtonTapped
+    case deleteButtonTapped
 }
 
-struct ListEnvironment {}
+struct ListEnvironment {
+    var persistence: PersistenceController
+}
 
 let listReducer = Reducer<
     ListState,
     ListAction,
     ListEnvironment
 > { state, action, environment in
-    return .none
+    switch action {
+    case .onAppear:
+        let items = environment.persistence.items()
+        let listItems = items.map(ListItem.init)
+        return Effect(value: .fetched(items: listItems))
+        
+    case .fetched(let items):
+        state.items = items
+        return .none
+        
+    case .settingsButtonTapped:
+        state.isSettingsPresented.toggle()
+        return .none
+        
+    case .deleteButtonTapped:
+        state.isDeletePresented.toggle()
+        return .none
+    }
 }
