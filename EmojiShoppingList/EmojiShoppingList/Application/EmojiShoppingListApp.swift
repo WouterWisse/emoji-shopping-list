06423e9748eb2fc25c86c652720228c52e7601b2
Wouter Wisse <hello@wouterwisse.com>
2022-05-27 16:12:10 -0400

    Setup store

--- EmojiShoppingList/EmojiShoppingList/Application/EmojiShoppingListApp.swift
+++ EmojiShoppingList/EmojiShoppingList/Application/EmojiShoppingListApp.swift
@@ -1,10 +1,17 @@
+import ComposableArchitecture
 import SwiftUI
 
 @main
 struct EmojiShoppingListApp: App {
     var body: some Scene {
         WindowGroup {
-            ListView()
+            TheListView(
+                store: Store(
+                    initialState: ListState(),
+                    reducer: listReducer,
+                    environment: ListEnvironment(persistence: PersistenceController.default)
+                )
+            )
         }
     }
 }
