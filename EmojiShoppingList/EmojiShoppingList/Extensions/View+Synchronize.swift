19c0dec1923dfbe5b72e1cd235d1707c056d2e8e
Wouter Wisse <hello@wouterwisse.com>
2022-05-28 13:12:52 -0400

    Add view extension to sync bindings

---
+++ EmojiShoppingList/EmojiShoppingList/Extensions/View+Synchronize.swift
@@ -0,0 +1,21 @@
+import SwiftUI
+
+extension View {
+    func synchronize<Value: Equatable>(
+        _ first: Binding<Value>,
+        _ second: Binding<Value>
+    ) -> some View {
+        self
+            .onChange(of: first.wrappedValue) { second.wrappedValue = $0 }
+            .onChange(of: second.wrappedValue) { first.wrappedValue = $0 }
+    }
+    
+    func synchronize<Value: Equatable>(
+        _ first: Binding<Value>,
+        _ second: FocusState<Value>.Binding
+    ) -> some View {
+        self
+            .onChange(of: first.wrappedValue) { second.wrappedValue = $0 }
+            .onChange(of: second.wrappedValue) { first.wrappedValue = $0 }
+    }
+}
