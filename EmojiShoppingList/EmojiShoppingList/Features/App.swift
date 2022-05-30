6682e838de4cc623a99b6305b551db4bbeaf61cc
Wouter Wisse <hello@wouterwisse.com>
2022-05-30 11:48:50 -0400

    Implement app reducer

--- EmojiShoppingList/EmojiShoppingList/Features/App.swift
+++ EmojiShoppingList/EmojiShoppingList/Features/App.swift
@@ -1,40 +1,92 @@
 import ComposableArchitecture
 import SwiftUI
 
-@main
-struct EmojiShoppingListApp: App {
-    var body: some Scene {
-        WindowGroup {
-            NavigationView {
-                ListView(
-                    store: Store(
-                        initialState: ListState(),
-                        reducer: listReducer,
-                        environment: ListEnvironment(
-                            persistence: .default,
-                            mainQueue: { .main }
-                        )
-                    )
-                )
-            }
-            // TODO: Add navigation buttons and logic here.
-        }
-    }
-}
+// MARK: - Logic
 
 struct AppState: Equatable {
     var listState = ListState()
+    var isSettingsPresented: Bool = false
 }
 
 enum AppAction {
     case listAction(ListAction)
+    case setSettings(isPresented: Bool)
+    case settingsButtonTapped
 }
 
 struct AppEnvironment {}
 
-let appReducer = Reducer<AppState, AppAction, AppEnvironment> { state, action, environment in
-    switch action {
-    case .listAction(let listAction):
-        return .none
+let appReducer = Reducer<AppState, AppAction, AppEnvironment>.combine(
+    listReducer.pullback(
+        state: \.listState,
+        action: /AppAction.listAction,
+        environment: { _ in ListEnvironment(persistence: .default, mainQueue: { .main }) }
+    ),
+    Reducer { state, action, environment in
+        switch action {
+        case .listAction(let listAction):
+            return .none
+            
+        case .setSettings(let isPresented):
+            state.isSettingsPresented = isPresented
+            return .none
+            
+        case .settingsButtonTapped:
+            state.isSettingsPresented.toggle()
+            return .none
+        }
+    }
+)
+
+// MARK: - View
+
+@main
+struct EmojiShoppingListApp: App {
+    let store: Store<AppState, AppAction> = Store(
+        initialState: AppState(),
+        reducer: appReducer,
+        environment: AppEnvironment()
+    )
+    
+    var body: some Scene {
+        WithViewStore(self.store) { viewStore in
+            WindowGroup {
+                NavigationView {
+                    ListView(
+                        store: self.store.scope(
+                            state: \.listState,
+                            action: AppAction.listAction
+                        )
+                    )
+                    .sheet(isPresented: viewStore.binding(
+                        get: { $0.isSettingsPresented },
+                        send:  AppAction.setSettings(isPresented:))
+                    ) {
+                        SettingsView()
+                    }
+                    .toolbar {
+                        ToolbarItem(placement: .navigationBarLeading) {
+                            Button {
+                                withAnimation {
+                                    viewStore.send(.settingsButtonTapped)
+                                }
+                            } label: {
+                                Image(systemName: "gearshape")
+                            }
+                        }
+                        ToolbarItem(placement: .navigationBarTrailing) {
+                            Button {
+                                withAnimation {
+                                    //                                viewStore.send(.deleteButtonTapped)
+                                }
+                            } label: {
+                                //                            Image(systemName: viewStore.isDeletePresented ? "trash.slash" : "trash")
+                            }
+                        }
+                    }
+                }
+                // TODO: Add navigation buttons and logic here.
+            }
+        }
     }
 }
