b917da673b997389991f537f67c487518b881acc
Wouter Wisse <hello@wouterwisse.com>
2022-05-31 14:54:46 -0400

    Add shared environment

---
+++ EmojiShoppingList/EmojiShoppingList/Features/SharedEnvironment.swift
@@ -0,0 +1,40 @@
+import ComposableArchitecture
+
+@dynamicMemberLookup
+struct SharedEnvironment<Environment> {
+    var environment: Environment
+    
+    var mainQueue: () -> AnySchedulerOf<DispatchQueue>
+    var persistence: () -> PersistenceController
+}
+
+// MARK: Environments
+
+extension SharedEnvironment {
+    static func live(environment: Environment) -> Self {
+        Self(
+            environment: environment,
+            mainQueue: { .main },
+            persistence: { .default }
+        )
+    }
+    
+    static func mock(environment: Environment) -> Self {
+        Self(
+            environment: environment,
+            mainQueue: { .main },
+            persistence: { .mock }
+        )
+    }
+}
+
+// MARK: Subscript
+
+extension SharedEnvironment {
+    subscript<Dependency>(
+        dynamicMember keyPath: WritableKeyPath<Environment, Dependency>
+    ) -> Dependency {
+        get { self.environment[keyPath: keyPath] }
+        set { self.environment[keyPath: keyPath] = newValue }
+    }
+}
