e8b5e8b44ac0f0b0cbce701ad94c20d964334617
Wouter Wisse <hello@wouterwisse.com>
2022-05-31 15:35:37 -0400

    Add feedback generator

---
+++ EmojiShoppingList/EmojiShoppingList/Shared/FeedbackGenerator.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+struct FeedbackGenerator {
+    var impact: (_ feedbackStyle: UIImpactFeedbackGenerator.FeedbackStyle) -> Void
+    var notify: (_ feedbackType: UINotificationFeedbackGenerator.FeedbackType) -> Void
+}
+
+extension FeedbackGenerator {
+    static let `default`: () -> FeedbackGenerator = {
+        let notificationFeedbackGenerator = UINotificationFeedbackGenerator()
+        return FeedbackGenerator(
+            impact: { feedbackStyle in
+                UIImpactFeedbackGenerator(style: feedbackStyle).impactOccurred()
+            },
+            notify: { feedbackType in
+                notificationFeedbackGenerator.notificationOccurred(feedbackType)
+            }
+        )
+    }
+}
