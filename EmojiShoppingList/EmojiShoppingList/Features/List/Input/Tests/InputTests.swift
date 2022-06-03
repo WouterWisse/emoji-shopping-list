ddca2981e6187ee2e7b9c6646e8b983e36733688
Wouter Wisse <hello@wouterwisse.com>
2022-06-03 19:20:31 -0400

    Setup tests

--- EmojiShoppingList/EmojiShoppingList/Features/List/Input/Tests/InputTests.swift
+++ EmojiShoppingList/EmojiShoppingList/Features/List/Input/Tests/InputTests.swift
@@ -3,15 +3,73 @@ import XCTest
 @testable import Shopping_List
 
 final class InputTests: XCTestCase {
+    
     let scheduler = DispatchQueue.test
+    var mockSettingsPersistence: MockSettingsPerstence!
+    var mockFeedbackGenerator: MockFeedbackGenerator!
+    
+    // MARK: SetUp / TearDown
+    
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        mockSettingsPersistence = MockSettingsPerstence()
+        mockFeedbackGenerator = MockFeedbackGenerator()
+    }
+    
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+        mockSettingsPersistence = nil
+        mockFeedbackGenerator = nil
+    }
     
     // MARK: Tests
     
-    func test_submit_shouldInvokeFeedback() {
+    func test_dismissKeyboard() {
         let store = TestStore(
-            initialState: InputState(),
+            initialState: InputState(
+                focusedField: .input
+            ),
             reducer: inputReducer,
-            environment: .mock(environment: InputEnvironment())
+            environment: .mock(
+                environment: InputEnvironment(),
+                mainQueue: self.scheduler.eraseToAnyScheduler(),
+                settingsPersistence: mockSettingsPersistence,
+                feedbackGenerator: mockFeedbackGenerator
+            )
         )
+        
+        store.send(.dismissKeyboard) { state in
+            state.focusedField = nil
+            XCTAssertEqual(
+                self.mockFeedbackGenerator.invokedImpactCount,
+                1
+            )
+            XCTAssertEqual(
+                self.mockFeedbackGenerator.invokedImpactParameters?.feedbackStyle,
+                .soft
+            )
+        }
+    }
+    
+    func test_prepareForNextItem() {
+        let store = TestStore(
+            initialState: InputState(
+                inputText: "Broccoli",
+                focusedField: .input,
+                isFirstFieldFocus: true
+            ),
+            reducer: inputReducer,
+            environment: .mock(
+                environment: InputEnvironment(),
+                mainQueue: self.scheduler.eraseToAnyScheduler(),
+                settingsPersistence: mockSettingsPersistence,
+                feedbackGenerator: mockFeedbackGenerator
+            )
+        )
+        
+        store.send(.prepareForNextItem) {
+            $0.isFirstFieldFocus = false
+            $0.inputText = ""
+        }
     }
 }
--- EmojiShoppingList/EmojiShoppingList/Shared/Mock/SharedEnvironment+Mock.swift
+++ EmojiShoppingList/EmojiShoppingList/Shared/Mock/SharedEnvironment+Mock.swift
@@ -1,13 +1,18 @@
-import Foundation
+import ComposableArchitecture
 @testable import Shopping_List
 
 extension SharedEnvironment {
-    static func mock(environment: Environment) -> Self {
+    static func mock(
+        environment: Environment,
+        mainQueue: AnySchedulerOf<DispatchQueue>,
+        settingsPersistence: MockSettingsPerstence,
+        feedbackGenerator: MockFeedbackGenerator
+    ) -> Self {
         Self(
             environment: environment,
-            mainQueue: { .main },
-            settingsPersistence: { .default },
-            feedbackGenerator: { .mock(MockFeedbackGenerator()) }
+            mainQueue: { mainQueue },
+            settingsPersistence: { .mock(settingsPersistence) },
+            feedbackGenerator: { .mock(feedbackGenerator) }
         )
     }
 }
