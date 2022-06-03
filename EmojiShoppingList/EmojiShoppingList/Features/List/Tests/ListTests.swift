40e47f204fa106c73bccf7f8e8e00f86d7289ec3
Wouter Wisse <hello@wouterwisse.com>
2022-06-03 19:45:31 -0400

    Add mock persistence controller

--- EmojiShoppingList/EmojiShoppingList/Features/List/Tests/ListTests.swift
+++ EmojiShoppingList/EmojiShoppingList/Features/List/Tests/ListTests.swift
@@ -9,6 +9,7 @@ final class ListTests: XCTestCase {
     let scheduler = DispatchQueue.test
     var mockSettingsPersistence: MockSettingsPerstence!
     var mockFeedbackGenerator: MockFeedbackGenerator!
+    var mockPersistenceController: MockPersistenceController!
     
     // MARK: SetUp / TearDown
     
@@ -16,12 +17,14 @@ final class ListTests: XCTestCase {
         try super.setUpWithError()
         mockSettingsPersistence = MockSettingsPerstence()
         mockFeedbackGenerator = MockFeedbackGenerator()
+        mockPersistenceController = MockPersistenceController()
     }
     
     override func tearDownWithError() throws {
         try super.tearDownWithError()
         mockSettingsPersistence = nil
         mockFeedbackGenerator = nil
+        mockPersistenceController = nil
     }
     
     // MARK: Tests
@@ -36,12 +39,13 @@ final class ListTests: XCTestCase {
                 createdAt: Date()
             )
         ]
+        mockPersistenceController.stubbedItemsResult = listItems
         
         let store = TestStore(
             initialState: ListState(),
             reducer: listReducer,
             environment: .mock(
-                environment: ListEnvironment(persistence: { .mock }),
+                environment: ListEnvironment(persistence: { .mock(self.mockPersistenceController) }),
                 mainQueue: self.scheduler.eraseToAnyScheduler(),
                 settingsPersistence: mockSettingsPersistence,
                 feedbackGenerator: mockFeedbackGenerator
