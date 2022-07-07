66da21bd24103d5849b51960076cd9eaa9e2dbd8
Wouter Wisse <hello@wouterwisse.com>
2022-07-07 13:48:02 -0400

    Add ColorTheme view

---
+++ EmojiShoppingList/Features/ColorTheme/ColorTheme.swift
@@ -0,0 +1,68 @@
+import SwiftUI
+import ComposableArchitecture
+
+// MARK: - Logic
+
+struct ColorThemeState: Equatable {
+    
+}
+
+enum ColorThemeAction: Equatable {
+    case onAppear
+}
+
+struct ColorThemeEnvironment {}
+
+let colorThemeReducer = Reducer<
+    ColorThemeState,
+    ColorThemeAction,
+    SharedEnvironment<ColorThemeEnvironment>
+> { state, action, environment in
+    switch action {
+    case .onAppear:
+        return .none
+    }
+}
+.debug()
+
+// MARK: - View
+
+struct ColorThemeView: View {
+    let store: Store<ColorThemeState, ColorThemeAction>
+    
+    var body: some View {
+        WithViewStore(self.store) { viewStore in
+            NavigationView {
+                List {
+                    Text("Hoi")
+                }
+                .onAppear {
+                    viewStore.send(.onAppear)
+                }
+                .listStyle(.insetGrouped)
+                .navigationTitle("Settings")
+            }
+        }
+    }
+}
+
+// MARK: Preview
+
+struct ColorThemeView_Previews: PreviewProvider {
+    static let colorSchemes: [ColorScheme] = [.light, .dark]
+    
+    static var previews: some View {
+        ForEach(colorSchemes, id: \.self) { colorScheme in
+            ColorThemeView(
+                store: Store(
+                    initialState: ColorThemeState(),
+                    reducer: colorThemeReducer,
+                    environment: .preview(
+                        environment: ColorThemeEnvironment()
+                    )
+                )
+            )
+            .preferredColorScheme(colorScheme)
+        }
+    }
+}
