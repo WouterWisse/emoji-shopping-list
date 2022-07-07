457c57d0c54c9233e8354d86cbe8c46aae632d84
Wouter Wisse <hello@wouterwisse.com>
2022-07-07 13:16:55 -0400

    Create color theme provider

---
+++ EmojiShoppingList/Shared/ColorThemeProvider.swift
@@ -0,0 +1,62 @@
+import SwiftUI
+
+enum ColorTheme: Int {
+    case primary
+    case red
+    case orange
+    case yellow
+    case green
+    case mint
+    case teal
+    case cyan
+    case blue
+    case indigo
+    case purple
+    case pink
+    case brown
+    case gray
+    
+    var color: Color {
+        switch self {
+        case .primary: return .primary
+        case .red: return .red
+        case .orange: return .orange
+        case .yellow: return .yellow
+        case .green: return .green
+        case .mint: return .mint
+        case .teal: return .teal
+        case .cyan: return .cyan
+        case .blue: return .blue
+        case .indigo: return .indigo
+        case .purple: return .purple
+        case .pink: return .pink
+        case .brown: return .brown
+        case .gray: return .gray
+        }
+    }
+    
+    var description: String {
+        switch self {
+        case .primary: return "Primary"
+        case .red: return "Red"
+        case .orange: return "Orange"
+        case .yellow: return "Yellow"
+        case .green: return "Green"
+        case .mint: return "Mint"
+        case .teal: return "Teal"
+        case .cyan: return "Cyan"
+        case .blue: return "Blue"
+        case .indigo: return "Indigo"
+        case .purple: return "Purple"
+        case .pink: return "Pink"
+        case .brown: return "Brown"
+        case .gray: return "Gray"
+        }
+    }
+}
+
+struct ColorThemeProvider {
+    func kijkMaarff() {
+        let color: Color = .brown
+    }
+}
