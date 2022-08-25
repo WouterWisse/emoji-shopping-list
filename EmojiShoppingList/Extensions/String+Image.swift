5188cdb281cbfe6925d4c6b925bad1cbc2475464
Wouter Wisse <wouter.wisse@persgroep.net>
2022-08-25 14:41:26 -0400

    Add seperate extensions

---
+++ EmojiShoppingList/Extensions/String+Image.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+extension String {
+    func toImage() -> UIImage? {
+        let nsString = (self as NSString)
+        let font = UIFont.systemFont(ofSize: 44)
+        let stringAttributes = [NSAttributedString.Key.font: font]
+        let imageSize = nsString.size(withAttributes: stringAttributes)
+        UIGraphicsBeginImageContextWithOptions(imageSize, false, 0)
+        UIColor.clear.set()
+        UIRectFill(CGRect(origin: CGPoint(), size: imageSize))
+        nsString.draw(at: CGPoint.zero, withAttributes: stringAttributes)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image ?? UIImage()
+    }
+}
